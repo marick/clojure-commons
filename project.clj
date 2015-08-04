0e38effe90cea023b3fd554eef7f178ea62defad
Brian Marick <marick@exampler.com>
2015-08-04 14:42:02 -0500

    use midje 1.8 and clojure 1.8

--- project.clj
+++ project.clj
@@ -11,11 +11,12 @@
                  [marick/suchwow "3.4.0" :exclusions [org.clojure/clojure]]
                  [slingshot "0.12.2"]]
 
-  :profiles {:dev {:dependencies [[midje "1.7.0" :exclusions [org.clojure/clojure]]]}
+  :profiles {:dev {:dependencies [[midje "1.8-alpha1" :exclusions [org.clojure/clojure]]]}
              :1.5.0 {:dependencies [[org.clojure/clojure "1.5.0"]]}
              :1.5.1 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0-alpha3"]]}
              }
 
   :plugins [[lein-midje "3.1.3"]
@@ -26,8 +27,8 @@
           :output-dir "/var/tmp/clojure-commons-doc"
           :defaults {:doc/format :markdown}}
 
-  :aliases {"compatibility" ["with-profile" "+1.5.0:+1.5.1:+1.6:+1.7" "midje" ":config" ".compatibility-test-config"]
-            "travis" ["with-profile" "+1.5.0:+1.5.1:+1.6:+1.7" "midje"]}
+  :aliases {"compatibility" ["with-profile" "+1.5.0:+1.5.1:+1.6:+1.7:+1.8" "midje" ":config" ".compatibility-test-config"]
+            "travis" ["with-profile" "+1.5.0:+1.5.1:+1.6:+1.7:+1.8" "midje"]}
 
   ;; For Clojure snapshots
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
