025b133ed26d9e745b5b7e3f48f012d87bc8019b
Brian Marick <marick@exampler.com>
2016-08-14 16:14:08 -0500

    run against latest clojure 1.9

--- project.clj
+++ project.clj
@@ -19,7 +19,7 @@
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
-             :1.9 {:dependencies [[org.clojure/clojure "1.9.0-alpha5"]]}
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0-alpha10"]]}
              }
 
   :plugins [[lein-midje "3.1.3"]
